9525aaf90f92b14d0e5ad1716085f9f1bd126596
collin-nunnally <157069516+collin-nunnally@users.noreply.github.com>
2025-02-06 16:07:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.c
 
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD9txyqjOcBIVkodnF_lKVYigtXXwGgvmk",
+  authDomain: "million-dollar-crew.firebaseapp.com",
+  projectId: "million-dollar-crew",
+  storageBucket: "million-dollar-crew.firebasestorage.app",
+  messagingSenderId: "719263246304",
+  appId: "1:719263246304:web:e2bf79dc335b9295c4ad85",
+  measurementId: "G-D7CB8TZ9C9"
 };
 
 // Initialize Firebase
